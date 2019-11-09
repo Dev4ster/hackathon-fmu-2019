350bc2c797b1fd2f1d28b3728ff87aee01bd2777
Fernando borges <fernandolook1@outlook.com>
2019-11-09 17:55:06 -0300

    tela inicial

---
+++ lib/PageHome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PageInitial extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      // Appbar
+      appBar: AppBar(
+        // Title
+        title: Text("Amiguíneos anúncio"),
+      ),
+      // Body
+      body: Container()
+      );
+    }
+  }
