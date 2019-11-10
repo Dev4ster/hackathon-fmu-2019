9db5e732a0385455f3417992835df78c153338f7
Fernando borges <fernandolook1@outlook.com>
2019-11-10 14:18:28 -0300

    Rota da tela de info do anúnciante

--- lib/ListAd.dart
+++ lib/ListAd.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-import 'AdInfo.dart';
+import 'main.dart';
 
 class Ad extends StatelessWidget {
   @override
@@ -9,54 +8,81 @@ class Ad extends StatelessWidget {
       padding: const EdgeInsets.all(4.0),
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
-          color: Colors.black12,
-          boxShadow: [
-            BoxShadow(
+            borderRadius: BorderRadius.circular(6),
             color: Colors.black12,
-            offset: Offset(1,2),
-            blurRadius: 2,
-            )
-          ]
-        ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(1, 2),
+                blurRadius: 2,
+              )
+            ]),
         width: MediaQuery.of(context).size.width,
         height: 180,
         child: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 15, right: 15, bottom: 15),
+          padding:
+              const EdgeInsets.only(top: 20, left: 15, right: 15, bottom: 15),
           child: Column(
             children: <Widget>[
               Text(
-                "Lorem Ipsum has been the industry's standard dummy.Lorem Ipsum has been the",
-                style: TextStyle(fontSize: 15,)
-              ),
+                  "Lorem Ipsum has been the industry's standard dummy.Lorem Ipsum has been the",
+                  style: TextStyle(
+                    fontSize: 15,
+                  )),
               Spacer(),
               Row(
                 children: <Widget>[
-                  Icon(Icons.stop, color: Colors.purple,),
-                  Text("Gato", style: TextStyle(fontSize: 15),),
+                  Icon(
+                    Icons.stop,
+                    color: Colors.purple,
+                  ),
+                  Text(
+                    "Gato",
+                    style: TextStyle(fontSize: 15),
+                  ),
                   Spacer(),
-                  Icon(Icons.pets, color: Colors.purple,),
-                  Text("Cão", style: TextStyle(fontSize: 15),),
+                  Icon(
+                    Icons.pets,
+                    color: Colors.purple,
+                  ),
+                  Text(
+                    "Cão",
+                    style: TextStyle(fontSize: 15),
+                  ),
                   Spacer(),
-                  Icon(Icons.stop, color: Colors.purple,),
-                  Text("Pássaro", style: TextStyle(fontSize: 15),),
+                  Icon(
+                    Icons.stop,
+                    color: Colors.purple,
+                  ),
+                  Text(
+                    "Pássaro",
+                    style: TextStyle(fontSize: 15),
+                  ),
                   Spacer(),
-                  Icon(Icons.room, color: Colors.purple,),
+                  Icon(
+                    Icons.room,
+                    color: Colors.purple,
+                  ),
                   Text("5 KM")
                 ],
               ),
               Container(
-              width: 180,
+                width: 180,
                 child: Padding(
                   padding: EdgeInsets.only(top: 10),
                   child: RaisedButton(
-                  color: Colors.purple,
-                  child: Text("Infomações"),
-                  onPressed:() {},
+                    color: Colors.purple,
+                    child: Text("Infomações"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AdInfo_route() ),
+                      );
+                    },
                   ),
                 ),
               )
-            
             ],
           ),
         ),
