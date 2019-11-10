a876198e0d4c0eb52e6e9f596350341c906a9db7
Dev4ster <victormenezes35@hotmail.com>
2019-11-10 09:17:57 -0300

    tela cad final

--- lib/TelaCadastro.dart
+++ lib/TelaCadastro.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
 class TelaCadastro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Containers Campos
+    TextEditingController enderecoController = new TextEditingController();
+    TextEditingController bairroController = new TextEditingController();
+    TextEditingController cidadeController = new TextEditingController();
+    
+
+
       String nome;
       String cpf;
       String email;
       String senha;
       String endereco;
       String numero;
+      String bairro;
       String cep;
       String cidade;
       String estado;
@@ -94,6 +104,167 @@ class TelaCadastro extends StatelessWidget {
           senha = valor;
         },
       );
+
+      TextField EnderecoField = new TextField(
+        controller: enderecoController,
+        decoration: InputDecoration(
+          labelText: "Endereço",
+           prefixIcon: Icon(Icons.room,color: Colors.white,),
+           labelStyle: new TextStyle(color: Colors.white,
+                                      fontSize: 18,
+                                  ),
+           enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, 
+                                  width: 1.0 ),
+           ),  
+        ),
+         style: TextStyle(
+        color: Colors.white
+      ),
+        onChanged: (valor){
+          senha = valor;
+         
+        },
+      );
+
+      //numero linha 
+      TextField NumeroField = new TextField(
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10),
+              labelText: "Número",
+              prefixIcon: Icon(Icons.map,color: Colors.white,),
+               labelStyle: new TextStyle(color: Colors.white,
+                                      fontSize: 18,
+                                  ),
+                      enabledBorder: new UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1.0 ),
+           ),  
+          ),
+           style: TextStyle(
+           color: Colors.white
+        ),
+        onChanged: (valor){
+          numero = valor;
+        },
+      );
+      //fim numero inha
+  TextField BairroField = new TextField(
+    controller: bairroController,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10),
+              labelText: "Bairro",
+              prefixIcon: Icon(Icons.rounded_corner,color: Colors.white,),
+               labelStyle: new TextStyle(color: Colors.white,
+                                      fontSize: 18,
+                                  ),
+           enabledBorder: new UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1.0 ),
+           ),                         
+          ),
+          style: TextStyle(
+           color: Colors.white
+        ),
+        onChanged: (valor){
+          bairro = valor;
+        },
+      );
+
+      TextField CepField = new TextField(
+        decoration: InputDecoration(
+          labelText: "CEP",
+           prefixIcon: Icon(Icons.pin_drop,color: Colors.white,),
+           labelStyle: new TextStyle(color: Colors.white,
+                                      fontSize: 18,
+                                  ),
+           enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, 
+                                  width: 1.0 ),
+           ),  
+        ),
+         style: TextStyle(
+        color: Colors.white
+      ),
+        onChanged: (valor) async{
+          cep = valor;
+          String api = "http://apps.widenet.com.br/busca-cep/api/cep.json?code="+cep;
+            var response = await http.get(api);
+            var dados = convert.json.decode(response.body);
+            enderecoController.text = dados["address"];
+            bairroController.text = dados["district"];
+            cidadeController.text =  dados["city"];
+          
+        },
+      );
+
+
+      TextField CidadeField = new TextField(
+        controller: cidadeController,
+        decoration: InputDecoration(
+          labelText: "Cidade",
+           prefixIcon: Icon(Icons.pin_drop,color: Colors.white,),
+           labelStyle: new TextStyle(color: Colors.white,
+                                      fontSize: 18,
+                                  ),
+           enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, 
+                                  width: 1.0 ),
+           ),  
+        ),
+         style: TextStyle(
+        color: Colors.white
+      ),
+      
+        onChanged: (valor){
+          cidade = valor;
+        },
+      );
+    
+  Row linhaNumeroBairro = new Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: <Widget>[
+    new Flexible(
+      child: NumeroField
+    ),
+    SizedBox(width: 20.0,),
+    new Flexible(
+      child: BairroField
+    ),
+  ],
+);
+
+Row linhaCepECidade = new Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: <Widget>[
+    new Flexible(
+      child: CepField
+    ),
+    SizedBox(width: 20.0,),
+    new Flexible(
+      child: CidadeField
+    ),
+  ],
+);
+
+   Container CadastrarButtom =  new Container(  
+       margin: const EdgeInsets.only(top: 20.0),
+      width: double.infinity,
+      height: 50,
+
+      // height: double.infinity,
+      child: new RaisedButton(
+          child:new Text("Cadastrar",
+          
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),),//dentro do botao tem que ter outro widget de texto
+          color:Colors.purple[900],
+          onPressed: (){
+            
+          },
+        ),
+);
+
     // fim campos
 Container Logo =  new Container(
       child: Text(
@@ -137,8 +308,11 @@ Container Logo =  new Container(
             CPFField,
             EmailField,
             SenhaField,
-            SenhaField,
-            SenhaField],
+            EnderecoField,
+            linhaNumeroBairro,
+            linhaCepECidade,
+            CadastrarButtom
+            ],
             
         ),
         
--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
 
     return TelaCadastro();
   }
